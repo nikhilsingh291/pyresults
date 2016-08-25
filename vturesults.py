c28b95c52aa52162dccadd41317db0429e1e84c0
nikhilsingh291 <nikhilsingh291@gmail.com>
2016-08-26 02:13:54 +0530

    vturesult.py added

---
+++ vturesults.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+u="1mv15cs"
+for i in range(001,135):
+    if (i<10):
+        pre='00'
+    elif (i<100):
+        pre='0'
+    else:
+        pre=''
+    no=str(i)
+    usn=u+pre+no
+    url="http://result.vtu.ac.in/cbcs_results2016.aspx?usn="+usn+"&sem=2"
+    reqfile = requests.get(url)
+    soup = BeautifulSoup(reqfile.content,"html5lib")
+    #print soup.prettify()
+    #print soup.title.text
+    if(soup.title.text=="cbcs_result"):
+        continue
+    print usn
+    print soup.find(id="txtName")['value'],soup.find(id="lblSGPA").text 
+    print ''
+  
