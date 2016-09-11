620307250022053432648a57057f8ad2f578b371
Nikhil Singh <nikhilsingh291@gmail.com>
2016-09-11 21:57:46 +0530

    Update vturesults.py

--- vturesults.py
+++ vturesults.py
@@ -1,7 +1,9 @@
+# Scrapes result from http://result.vtu.ac.in using BeautifulSoup
+
 from bs4 import BeautifulSoup
 import requests
-u="1mv15cs"
-for i in range(1,135):
+u="1mv15cs" # beginning part of the USN. Without serial  
+for i in range(1,135): # USN range for every branch
     if (i<10):
         pre='00'
     elif (i<100):
@@ -9,7 +11,7 @@ for i in range(1,135):
     else:
         pre=''
     no=str(i)
-    usn=u+pre+no
+    usn=u+pre+no # Concatenated USN
     url="http://result.vtu.ac.in/cbcs_results2016.aspx?usn="+usn+"&sem=2"
     reqfile = requests.get(url)
     soup = BeautifulSoup(reqfile.content,"html5lib")
@@ -18,5 +20,5 @@ for i in range(1,135):
     if(soup.title.text=="cbcs_result"):
         continue
     print usn
-    print soup.find(id="txtName")['value'],soup.find(id="lblSGPA").text
+    print soup.find(id="txtName")['value'],soup.find(id="lblSGPA").text # print USN, Name and GPA
     print ''
